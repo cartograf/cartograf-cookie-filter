df5f44ecae2742ae4177ab7fc7f84db423475ce6
cartograf <info@cartograf.net>
2014-01-11 19:45:44 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,111 @@
+<?php
+function cg_cf_cookie_accepted(){
+    return !empty($_COOKIE['cg_cookie_accepted']);
+}
+
+function cg_cf_cookie_bar(){
+    $text = get_option('cg_cf_text');
+    ?>
+        <div id="cg_cookie_bar" class="<?php echo cg_cf_is_exception() ? 'exception' : '' ?>" data-timeout="<?php cg_cf_timeout();?>" data-scrollout="<?php cg_cf_scrollout();?>">
+            <span class="close">&times;</span>
+            <?php echo $text; ?>
+        </div>
+        <style>
+            <?php echo file_get_contents(dirname(__FILE__).'/style.css')?>
+        </style>
+    <?php
+}
+
+function cg_cf_code_templates(){
+    $replace = array(
+        'from'  => array('/*',  '*/',   '</script'),
+        'to'    => array('/!*', '*!/',  '<!/script')
+    );
+
+    echo '<script type="text/template" id="cg_cf_head_template">/*!* ';
+        echo str_replace($replace['from'],$replace['to'],cg_cf_get_head_accepted());
+    echo ' *!*/</script>';
+    echo '<script type="text/template" id="cg_cf_foot_template">/*!* ';
+        echo str_replace($replace['from'],$replace['to'],cg_cf_get_foot_accepted());
+    echo ' *!*/</script>';
+}
+
+function cg_cf_get_head_accepted(){
+    $code = get_option('cg_cf_head_accepted_code');
+    return $code;
+}
+
+function cg_cf_head_accepted(){
+    echo cg_cf_get_head_accepted();
+}
+
+function cg_cf_get_head_denied(){
+    $code = get_option('cg_cf_head_denied_code');
+    return $code;
+}
+
+function cg_cf_head_denied(){
+    echo cg_cf_get_head_denied();
+}
+
+function cg_cf_get_foot_accepted(){
+    $code = get_option('cg_cf_foot_accepted_code');
+    return $code;
+}
+
+function cg_cf_foot_accepted(){
+    echo cg_cf_get_foot_accepted();
+}
+
+function cg_cf_get_foot_denied(){
+    $code = get_option('cg_cf_foot_denied_code');
+    return $code;
+}
+
+function cg_cf_foot_denied(){
+    echo cg_cf_get_foot_denied();
+}
+
+function cg_cf_get_timeout(){
+    $timeout = (int)get_option('cg_cf_accept_timeout');
+    return $timeout;
+}
+function cg_cf_timeout(){
+    echo cg_cf_get_timeout();
+}
+
+function cg_cf_get_scrollout(){
+    $scrollout = (int)get_option('cg_cf_accept_scrollout');
+    return $scrollout;
+}
+function cg_cf_scrollout(){
+    echo cg_cf_get_scrollout();
+}
+
+function cg_cf_get_exceptions(){
+    $items = explode(',',get_option('cg_cf_exception_pages'));
+    for ($i = 0; $i < count($items); $i++){
+        $items[$i] = trim($items[$i]);
+    }
+    return $items;
+}
+
+function cg_cf_is_exception(){
+    $return = false;
+
+    $exceptions = cg_cf_get_exceptions();
+    $obj = get_queried_object();
+    if ($obj && (
+            (isset($obj->post_name) && in_array($obj->post_name,$exceptions))
+            ||
+            (isset($obj->ID) && in_array($obj->ID,$exceptions))
+            ||
+            (isset($obj->post_title) && in_array($obj->post_title,$exceptions))
+        )
+    ){
+        $return = true;
+    }
+    return $return;
+}
+
+?>
