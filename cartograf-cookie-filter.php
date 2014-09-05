5214d78e796a91b1c78abc9458f7e3a9beb0964c
Jose Alcántara <jose@cartograf.net>
2014-09-05 18:44:28 +0200

    Tested with WordPress 4.0

--- cartograf-cookie-filter.php
+++ cartograf-cookie-filter.php
@@ -3,7 +3,7 @@
 Plugin Name: Cartograf Cookie filter
 Plugin URI: http://www.cartograf.net/plugin-wordpress-cumplir-ley-de-cookies-espana
 Description: Previene la instalación de cookies previa al consentimiento informado de los visitantes
-Version: 1.1.1
+Version: 1.1.2
 Author: Cartograf
 Author URI: http://www.cartograf.net
 Author Email: info@cartograf.net
